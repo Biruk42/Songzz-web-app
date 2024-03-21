55960d8cf40c3e69cf8a90b0d1d531bc8023d957
Biruk Kassaye <kassayebiruk0@gmail.com>
2024-03-21 17:51:15 +0300

    Add a related song section at the bottom of the detail page

---
+++ src/components/RelatedSong.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Card from "./Card";
+
+const RelatedSong = ({ songs, id }) => {
+  return (
+    <div>
+      <div className="blog-heading text-start pt-3 py-2 mb-4">
+        Related Songs
+      </div>
+      <div className="col-md-12 text-left justify-content-center">
+        <div className="row gx-5">
+          {songs.length === 1 && (
+            <h5 className="text-center">
+              No related songs found with this current song
+            </h5>
+          )}
+          {songs
+            ?.filter((songs) => songs.id !== id)
+            .map((item) => (
+              <Card {...item} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RelatedSong;
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,16 +1,25 @@
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  query,
+  where,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase";
 import Tags from "../components/Tags";
 import MostPopular from "../components/MostPopular";
+import RelatedSong from "../components/RelatedSong";
 
 const Detail = ({ setActive }) => {
   const { id } = useParams();
   const [song, setSong] = useState(null);
   const [songs, setSongs] = useState([]);
   const [tags, setTags] = useState([]);
-
+  const [relatedSongs, setRelatedSongs] = useState([]);
   useEffect(() => {
     const getSongsData = async () => {
       const songRef = collection(db, "songs");
@@ -29,11 +38,28 @@ const Detail = ({ setActive }) => {
   }, [id]);
 
   const getSongDetail = async () => {
+    const songRef = collection(db, "songs");
     const docRef = doc(db, "songs", id);
     const songDetail = await getDoc(docRef);
+    const songs = await getDocs(songRef);
+    let tags = [];
+    songs.docs.map((doc) => tags.push(...doc.get("tags")));
+    let uniqueTags = [...new Set(tags)];
+    setTags(uniqueTags);
     setSong(songDetail.data());
+    const relatedSongsQuery = query(
+      songRef,
+      where("tags", "array-contains-any", songDetail.data().tags, limit(3))
+    );
+    const relatedSongSnapshot = await getDocs(relatedSongsQuery);
+    const relatedSongs = [];
+    relatedSongSnapshot.forEach((doc) => {
+      relatedSongs.push({ id: doc.id, ...doc.data() });
+    });
+    setRelatedSongs(relatedSongs);
     setActive(null);
   };
+  console.log(relatedSongs);
   return (
     <div className="single">
       <div
@@ -54,16 +80,17 @@ const Detail = ({ setActive }) => {
                 {song?.timestamp.toDate().toDateString()}
               </span>
               <p className="text-start">{song?.description}</p>
-              {/* <div className="text-start">
+              <div className="text-start">
                 <Tags tags={song?.tags} />
-              </div> */}
+              </div>
             </div>
             <div className="col-md-3">
-              {/* <div className="blog-heading text-start py-2 mb-4">Tags</div> */}
+              <div className="blog-heading text-start py-2 mb-4">Tags</div>
               <Tags tags={tags} />
               <MostPopular title={"Recent Songs"} songs={songs} />
             </div>
           </div>
+          <RelatedSong id={id} songs={relatedSongs} />
         </div>
       </div>
     </div>
