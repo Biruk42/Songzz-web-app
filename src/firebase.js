d8607369bfa6be3e268cd56b19ac484284cb685a
Biruk <109275063+Biruk42@users.noreply.github.com>
2024-03-23 07:25:12 +0300

    fix bugs

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APP_ID,
+ apiKey: AIzaSyCJwV749i7icBPw9vjvTJ9B9iZkZCsdwAE,
+  authDomain: react-song-app.firebaseapp.com,
+  projectId: react-song-app,
+  storageBucket: react-song-app.appspot.com,
+  messagingSenderId: 217837909174,
+  appId: 1:217837909174:web:3a0d37cac5c6f6ed032217,
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
