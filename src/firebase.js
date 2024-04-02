6a55cb682b28eeba23027029b8ee58786c637996
Biruk <109275063+Biruk42@users.noreply.github.com>
2024-04-02 19:49:45 +0300

    Update firebase.js to hide the apiKey

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCJwV749i7icBPw9vjvTJ9B9iZkZCsdwAE",
-  authDomain: "react-song-app.firebaseapp.com",
+  apiKey: "apiKey",
+  authDomain: "Domain",
   projectId: "react-song-app",
-  storageBucket: "react-song-app.appspot.com",
-  messagingSenderId: "217837909174",
-  appId: "1:217837909174:web:3a0d37cac5c6f6ed032217",
+  storageBucket: "Bucket",
+  messagingSenderId: "senderId",
+  appId: "appId",
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
